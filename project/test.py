1611d37ead96c45972561f32250bc63ad60753ee
两个星 <57798670+woshiwangzihao@users.noreply.github.com>
2023-03-18 10:28:45 +0800

    Add files via upload

---
+++ project/test.py
@@ -0,0 +1,48 @@
+
+import torchvision
+from PIL.Image import Image
+from torch.utils.tensorboard import SummaryWriter
+from PIL import Image
+from model import *
+# 准备数据集
+from torch import nn
+from torch.utils.data import DataLoader
+
+image_path = "./imgs/airplane.png"
+image = Image.open(image_path)
+print(image)
+image = image.convert('RGB')
+transform = torchvision.transforms.Compose([torchvision.transforms.Resize((32, 32)),
+                                            torchvision.transforms.ToTensor()])
+
+image = transform(image)
+print(image.shape)
+
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64*4*4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+model = torch.load("tudui_0.pth", map_location=torch.device('cpu'))
+print(model)
+image = torch.reshape(image, (1, 3, 32, 32))
+model.eval()
+with torch.no_grad():
+    output = model(image)
+print(output)
+
+print(output.argmax(1))
